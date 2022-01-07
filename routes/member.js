5bb2243875f4a74f36e22c0106b334e91b451002
Chavi Surujbhali <surujbhalichavi@gmail.com>
2022-01-07 21:57:18 +0400

    fix 1

--- routes/member.js
+++ routes/member.js
@@ -14,4 +14,11 @@ router.get('/accept', member.accform);
 router.post('/accept', member.acceptMembers);
 router.get('/download', member.excel);
 
+//fix
+const member = require("../models/member")
+router.post('/list/:no', async (req, res) => {
+    var members = await member.find({}).sort({ _id: -1 }).limit(req.params.no);
+    res.json(members);
+})
+
 module.exports = router
